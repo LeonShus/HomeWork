8df63a3333ab601fa20772bf3cf4ecd639187006
Leonid Shustov <leonshustov1995@gmail.com>
2021-10-14 19:05:19 +0300

    Create HomeWork

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type MessageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Message
