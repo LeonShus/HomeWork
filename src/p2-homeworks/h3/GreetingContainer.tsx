9de9539d6c43f9ef3917d0d0db452436064a1dc1
Leonid Shustov <leonshustov1995@gmail.com>
2021-10-27 12:02:40 +0300

    add HW3

--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,28 +1,31 @@
-import React, {useState} from 'react'
-import Greeting from './Greeting'
+import React, {ChangeEvent, useState} from "react"
+import Greeting from "./Greeting"
+import {UserType} from "./HW3";
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: Array<UserType>
+    addUserCallback: (name: string) => void
 }
 
-// более простой и понятный для новичков
-// function GreetingContainer(props: GreetingPropsType) {
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => {
+    const [name, setName] = useState<string>("")
+    const [error, setError] = useState<string>("")
 
-// более современный и удобный для про :)
-// уровень локальной логики
-const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        let val = e.currentTarget.value
+        setName(val)
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+        val.trim() ? setError("") : setError("Incorect name")
     }
+
+    let trimName = name.trim()
     const addUser = () => {
-        alert(`Hello  !`) // need to fix
+        alert(`Hello ${trimName}!`)
+        addUserCallback(trimName)
+        setName("")
     }
 
-    const totalUsers = 0 // need to fix
+    const totalUsers = users.length
 
     return (
         <Greeting
