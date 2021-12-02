53e6957ddc3eeb4eb07447de3239d469311868e7
LeonShus <leonshustov1995@gmail.com>
2021-12-02 10:53:36 +0300

    add HW8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -4,19 +4,20 @@ type ActionType = SortType | CheckType
 
 export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
-        case 'sort': {
+        case "sort": {
             const copyState = [...state]
-            if(action.payload === "up"){
-                copyState.sort((a,b) => a.name > b.name ? 1 : -1)
-            } else if (action.payload === "down"){
-                copyState.sort((a,b) => a.name < b.name ? 1 : -1)
+            if (action.payload === "up") {
+                copyState.sort((a, b) => a.name > b.name ? 1 : -1)
+            } else if (action.payload === "down") {
+                copyState.sort((a, b) => a.name < b.name ? 1 : -1)
             }
             return copyState
         }
-        case 'check': {
+        case "check": {
             return [...state.filter(el => el.age >= action.payload)]
         }
-        default: return state
+        default:
+            return state
     }
 }
 
