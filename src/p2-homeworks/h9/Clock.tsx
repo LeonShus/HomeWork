a55fb662d3c7a238745031a766c4b306ba9cb675
LeonShus <leonshustov1995@gmail.com>
2021-12-16 13:29:52 +0300

    Create timer and time + date div

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,16 +3,16 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
-    const [show, setShow] = useState<boolean>(false)
+    const [date, setDate] = useState<Date>(new Date)
+    const [show, setShow] = useState<boolean>(true)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date)
         }, 1000)
         setTimerId(id)
     }
@@ -24,8 +24,15 @@ function Clock() {
         // close
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const hours = date.getHours()
+    const min = date.getMinutes()
+    const sec = date.getSeconds()
+    const day = date.getDate()
+    const month = date.getMonth()
+    const year = date.getFullYear()
+
+    const stringTime = [hours, min, sec].map(el => el < 10 ? '0' + el : el).join(':') // fix with date
+    const stringDate = [day, month, year].map(el => el < 10 ? '0' + el : el).join('.') // fix with date
 
     return (
         <div>
